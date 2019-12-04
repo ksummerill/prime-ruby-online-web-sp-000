a65b6cd34d466da700801d7dfe21f9c82d623a9b
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-04 02:08:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# returns a boolean of whether or not that integer is a prime number.
+require 'prime'
+
+def prime?(integer)
+  integer.prime?
+end
+
+
+
+# What defines an integer as a prime number?
+# Research algorithms for how to determine if a number is prime.
+# How do you create a range of numbers?
+# How do you turn a range into an array so that it can be iterated over?
