bd632745336ca2792566f754bb98624ee545c0b7
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-04 02:08:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,3 @@ require 'prime'
 def prime?(integer)
   integer.prime?
 end
-
-
-
-# What defines an integer as a prime number?
-# Research algorithms for how to determine if a number is prime.
-# How do you create a range of numbers?
-# How do you turn a range into an array so that it can be iterated over?
