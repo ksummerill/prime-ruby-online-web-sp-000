42b43a81180cd2f0e38da0922e0898d7db0b13ac
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-04 02:17:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 # returns a boolean of whether or not that integer is a prime number.
 require 'prime'
+require 'benchmark'
 
 def prime?(integer)
-  integer.prime?
+puts Benchmark.measure {integer.prime?} 
 end
